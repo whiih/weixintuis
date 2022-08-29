f9531861615bd5f28b30d820af5d60b165b82313
whiih <112364247+whiih@users.noreply.github.com>
2022-08-29 20:34:31 +0800

    Add files via upload

---
+++ wechatpush-master/src/test/java/cn/cvzhanshi/wechatpush/WechatpushApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.cvzhanshi.wechatpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatpushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
